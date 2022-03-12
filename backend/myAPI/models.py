3d57f85111c598ac172c8f98b2fa7c05b1376a78
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-12 18:11:05 +0000

    Update models.py

--- backend/myAPI/models.py
+++ backend/myAPI/models.py
@@ -50,6 +50,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
