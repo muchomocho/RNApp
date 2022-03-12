03b5a7c49a9d06c355cc9e0cf765477269fc81e1
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-12 06:05:55 +0000

    added style for bottom tab component

--- MyApp/Components/BottonTab/BottomTab.js
+++ MyApp/Components/BottonTab/BottomTab.js
@@ -1,20 +1,67 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { StyleSheet } from 'react-native';
 import SignIn from '../../Screens/SignIn'
 import Recipe from '../../Screens/Recipe';
 import ClassTest from '../../Screens/ClassTest';
 import CreateRecipe from '../../Screens/CreateRecipe';
+import SignUp from '../../Screens/SignUp';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='SignIn' component={SignIn} />
-            <Tab.Screen name='ClassTest' component={ClassTest} />
+        <Tab.Navigator screenOptions={{
+            tabBarHideOnKeyboard: true,
+
+            tabBarStyle: {
+                position: 'absolute',
+                backgroundColor: '#fff',
+                height: 85,
+                borderRadius: 10,
+                margin: 10,
+            },
+
+            tabBarItemStyle: {
+                padding: 5
+            },
+
+            tabBarLabelStyle: {
+                color: '#2f7500',
+                fontSize: 12
+            },
+
+            tabBarActiveTintColor: '#123',
+        }}
+        >
+            <Tab.Screen 
+            name='Sign in' 
+            component={SignIn} 
+            options={{
+                tabBarLabel: 'Sign in',
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons name="home" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen name='Sign up' component={SignUp} />
             <Tab.Screen name='Recipe' component={Recipe} />
-            <Tab.Screen name='CreateRecipe' component={CreateRecipe} />
+            <Tab.Screen name='Create' component={CreateRecipe} />
         </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    bottomtab_shadow: {
+        shadowColor: '#fff',
+        shadowRadius: 0,
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 0,
+            height: 100
+        },
+    },
+});
 
 export default BottomTab
