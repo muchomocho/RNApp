5694ec47aee5cd03ab18793c45ad1f73b3240565
muchomocho <Ryo.Mochizuki@city.ac.uk>
2022-03-22 16:17:36 +0000

    Update Global.js

--- MyApp/Global/Global.js
+++ MyApp/Global/Global.js
@@ -1,7 +1,7 @@
 export class GlobalConstant {
     static localhost = 'http://127.0.0.1:8000/';
     static localNetwork = 'http://192.168.0.32:8081/';
-    static rootUrl = this.localNetwork
+    static rootUrl = this.localhost
     static username = ''
 }
 
