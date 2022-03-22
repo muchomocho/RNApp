9ecfed8461d3358b13d48eb071f92d4da068f945
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-22 02:23:13 +0000

    Update AccountProfile.js

--- MyApp/Screens/AccountProfile.js
+++ MyApp/Screens/AccountProfile.js
@@ -3,14 +3,14 @@ import { ActivityIndicator, StyleSheet, Text, View, Button, FlatList, Alert } fr
 import GlobalConstant from '../Global/Global'
 import * as Authentication from "../Authentication/Authentication";
 
-function Recipe({ navigation }) {
+function AccountProfile({ navigation }) {
 
     const [data, setData] = useState([]);
 
     // https://reactnative.dev/docs/network
-    const getUserData = async (token) => {
+    const getUserProfile = async (token) => {
         try {
-            const response = await fetch(GlobalConstant.rootUrl + '/api/userprofile/', {
+            const response = await fetch(GlobalConstant.rootUrl + 'api/userprofile/' + GlobalConstant.username + '/', {
                 method: "GET",
                 headers: {
                     Accept: 'application/json',
@@ -26,17 +26,13 @@ function Recipe({ navigation }) {
           } 
     };
 
-    const createUserAccount = async () => {
-
-    };
-
     // https://reactnavigation.org/docs/function-after-focusing-screen/
     useEffect(() => {
         const reload = navigation.addListener('focus', () => {
           // The screen is focused
           // Call any action
           Authentication.getStoredAccessToken()
-          .then((token) => getUserAccount(token));
+          .then((token) => getUserProfile(token));
         });
     
         // Return the function to unsubscribe from the event so it gets removed on unmount
@@ -45,37 +41,85 @@ function Recipe({ navigation }) {
 
 
     const styles = StyleSheet.create({
-        cardStyle: {
+        account:{
             width: '95%',
-            backgroundColor: '#eee',
-            borderRadius: 30,
-            padding: 10,
-            margin: 10
+            backgroundColor: '#561ddb',
+            borderRadius: 5,
+            padding: 20,
+            margin: 10,
+        },
+        accountText:{
+            color: '#fff'
+        },
+        people: {
+            width: '95%',
+            backgroundColor: '#fff',
+            borderRadius: 5,
+            padding: 20,
+            margin: 10,
+
+            elevation: 3,
+            shadowColor: '#eee',
+            shadowRadius: 0,
+            shadowOpacity: 0.2,
+            shadowOffset: {
+                width: 0,
+                height: 100
+            },
+        },
+        peopleDetailContainer:{
+            flexDirection: 'row',
+            borderBottomColor: '#000',
+            borderBottomWidth: 0
+        },
+        peopleTextName:{
+            fontSize: 20,
+        },
+        peopleText: {
+            marginLeft: 10,
         }
     });
 
     const renderData = (item) => {
         return(
-            <View style={styles.cardStyle}>
-                <Text>{item.id}</Text>
-                <Text>{item.user_ID}</Text>
-                <Text>{item.title}</Text>
-                <Text>{item.main_image_url}</Text>
+            <View style={styles.people}>
+                <Text style={styles.peopleTextName}>{item.name}</Text>
+                <View style={styles.peopleDetailContainer}>
+                    <Text>Age</Text>
+                    <Text style={styles.peopleText}>{item.age}</Text>
+                </View>
+                <View style={styles.peopleDetailContainer}>
+                    <Text>Gender</Text>
+                    <Text style={styles.peopleText}>{item.gender}</Text>
+                </View>
             </View>
         )
     }
 
     return(
         <FlatList
-        data = {data}
+        ListHeaderComponent={
+            <View style={styles.account}>
+                <Text style={styles.accountText}>
+                    {data.username}
+                </Text>
+                <Text style={styles.accountText}>
+                    {data.email}
+                </Text>
+            </View>
+        }
+        
+        data = {data.people}
         renderItem = {({item}) => {
             return renderData(item)
         }}
         keyExtractor = {item => `${item.id}`}
+        
         />
-)
+        
+    )
 
     
 }
 
-export default Recipe
+export default AccountProfile
