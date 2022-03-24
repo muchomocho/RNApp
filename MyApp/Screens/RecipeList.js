3e0a4314a114bf0c57945b5a304bfdb217221136
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-24 03:10:56 +0000

    Create RecipeList.js

---
+++ MyApp/Screens/RecipeList.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, StyleSheet, Text, View, Button, FlatList, Alert } from 'react-native';
+import { TouchableOpacity } from "react-native";
+import GlobalConstant from '../Global/Global'
+
+function RecipeList({ navigation }) {
+
+    const [data, setData] = useState([]);
+
+    // https://reactnative.dev/docs/network
+    const getRecipe = async () => {
+        try {
+            const response = await fetch(GlobalConstant.rootUrl + 'api/recipetitles/', {
+                method: "GET"
+            });
+            const json = await response.json();
+            setData(json);
+          } catch (error) {
+            console.error(error);
+          } 
+    }
+
+    // https://reactnavigation.org/docs/function-after-focusing-screen/
+    useEffect(() => {
+        const reload = navigation.addListener('focus', () => {
+          // The screen is focused
+          // Call any action
+          getRecipe();
+        });
+    
+        // Return the function to unsubscribe from the event so it gets removed on unmount
+        return reload;
+    }, [navigation]);
+
+    const renderData = (item) => {
+        return(
+            <TouchableOpacity 
+            style={styles.container}
+            onPress={() => {navigation.navigate('Recipe detail', {recipe: item});}}
+            >
+                <Text style={styles.image}>{item.id}</Text>
+                <View style={styles.detail}>
+                    <Text style={styles.detailTitle}>{item.title}</Text>
+                    <Text style={styles.detailText}>created by {item.user}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    return(
+        <FlatList
+        data = {data}
+        renderItem = {({item}) => {
+            return renderData(item)
+        }}
+        keyExtractor = {item => `${item.id}`}
+        
+        ListFooterComponent={
+            <Text></Text>
+        }
+        ListFooterComponentStyle={{marginBottom: 100}}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        height: 150,
+        width: '95%',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 10,
+        margin: 10,
+
+        elevation: 3,
+        shadowColor: '#eee',
+        shadowRadius: 0,
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 0,
+            height: 100
+        },
+    },
+    detail: {
+        marginLeft: 10,
+        flex: 1,
+        overflow: 'hidden'
+    },
+    detailTitle: {
+        flexWrap: 'wrap',
+        fontSize: 16,
+        overflow: 'hidden'
+    },
+    detailText: {
+        flexWrap: 'wrap',
+        overflow: 'hidden'
+    },
+    image: {
+        flexGrow: 1,
+
+        backgroundColor: '#eee',
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    },
+});
+
+export default RecipeList
