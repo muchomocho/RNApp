215890523986e0ff6413f6b60bb3a4bafee1b49b
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-27 02:43:01 +0100

    added search bar

---
+++ MyApp/Components/SearchBar/SearchBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Text, TextInput, View, StyleSheet, Pressable } from 'react-native';
+
+export default function SearchBar({value, setValue, onSearch}) {
+
+        return (
+        <View style={styles.container}>
+            <TextInput 
+            value={value} 
+            onChangeText={setValue} 
+            placeholder={'search...'}
+            style={[styles.input, styles.border]}
+            />
+            <Pressable 
+            onPress={onSearch} 
+            style={[styles.button, styles.border]}
+            >
+            </Pressable>
+        </View>
+    );
+
+    
+}
+
+const styles = StyleSheet.create({
+    input: {
+        flex: 8,
+        backgroundColor: '#fff',
+        paddingLeft: 15,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+        borderRightWidth: 0
+    },
+
+    button:{
+        flex: 2,
+        backgroundColor: '#561ddb',
+        width: 20,
+        borderBottomRightRadius: 5,
+        borderTopRightRadius: 5,
+        borderLeftWidth: 0
+    },
+
+    border: {
+        borderColor: '#888',
+        borderWidth: 1
+    },
+
+    container: {
+        flexDirection: 'row',
+        height: 70,
+        padding: 10,
+        borderColor: '#000',
+    }
+  });
+
