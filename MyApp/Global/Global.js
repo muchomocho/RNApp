fbe89b31834a9dee125ef5576dda63d8cef31561
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-29 03:42:52 +0100

    renamed files

--- MyApp/Global/Global.js
+++
@@ -1,8 +0,0 @@
-export class GlobalConstant {
-    static localhost = 'http://127.0.0.1:8000/';
-    static localNetwork = 'http://192.168.0.32:8081/';
-    static rootUrl = this.localNetwork
-    static username = ''
-}
-
-export default GlobalConstant
