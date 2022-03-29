b1013c9daf253a521d9e62b41228c6093846b6f0
muchomocho <ryo.mochizuki@city.ac.uk>
2022-03-29 03:44:29 +0100

    added graph

--- MyApp/Screens/UserRecords.js
+++ MyApp/Screens/UserRecords.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { ActivityIndicator, StyleSheet, Text, View, Button, FlatList, Alert, Dimensions } from 'react-native';
-import GlobalConstant from '../Global/Global'
-import CustomButton from "../Components/CustomButton";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as APIRequest from '../Request/APIRequest';
+import * as APIRequest from '../API/ServerRequest';
+import { dietDataContainer } from "../Constant/Constant";
 import {
     LineChart,
     BarChart,
@@ -14,81 +13,124 @@ import {
   } from 'react-native-chart-kit'
 
 // https://reactnative.dev/docs/navigation
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator(); 
 
 function UserRecord(props) {
     
     const [data, setData] = useState([]);
+    
+    const date = new Date();
 
     // https://reactnavigation.org/docs/function-after-focusing-screen/
     useEffect(() => {
         const reload = props.navigation.addListener('focus', async () => {
-            // The screen is focused
-            // Call any action
             try {
-                const result = await APIRequest.apiRequest({
-                method: 'GET',
-                endpoint: 'api/userrecords/?name=' + props.route.params.userdata.name + '/',
-                isAuthRequired: true,
-            });
-            console.log(result.json);
-            setData(result.json);
-        } catch (error) {
-            console.log('userrecord', error)
-        }
+                await getUserRecord();
+            } catch (error) {
+                console.log(error)
+            }
         });
     
         // Return the function to unsubscribe from the event so it gets removed on unmount
         return reload;
     }, [props]);
 
-    // format data acquired to map onto graph
-    const formatData = () => {
-
+    const getUserRecord = async () => {
+        try {
+            const result = await APIRequest.httpRequest({
+            method: 'GET',
+            endpoint: 'api/userrecords/?name=' 
+            + props.route.params.userdata.name 
+            //+ '?range='
+            ,isAuthRequired: true
+            });
+            
+            setData(result.json);
+        
+        } catch (error) {
+        console.log('userrecord', error)
+        }
     };
 
-    const barData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [
-          {
-            data: [20, 45, 28, 80, 99, 43],
-          },
-        ],
-      };
+    // format data acquired to map onto graph
+    const formatData = () => {
+        const currentMonthDays = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+        var labels = new Array(currentMonthDays);
+        for (var i = 0; i < currentMonthDays; i++) {
+            labels[i] = date.getFullYear() + '-' + '0' + (date.getMonth()+1) + '-' + (i + 1)
+        }
+
+        var dataContainerObj = dietDataContainer();
+        dataContainerObj.labels = labels;
+        for (var prop in dataContainerObj) {
+            if (Object.prototype.hasOwnProperty.call(dataContainerObj, prop)) {
+                dataContainerObj[prop].data = new Array(currentMonthDays).fill(0);
+            }
+        }
+
+        for (const dayData of data) {
+            const index = labels.indexOf(dayData["date"]);
+            if (index > -1) {
+                // https://stackoverflow.com/questions/8312459/iterate-through-object-properties
+                for (var prop in dayData) {
+                    if (Object.prototype.hasOwnProperty.call(dayData, prop)
+                    && Object.prototype.hasOwnProperty.call(dataContainerObj, prop)) {
+                        dataContainerObj[prop].data[index] = parseFloat(dayData[prop]); 
+                    }
+                }
+            }
+        }
+        return dataContainerObj;
+    };
+
+    const plot = () => {
+        const data = formatData();
+
+        return (
+            <LineChart
+                data={{
+                labels: data.labels,
+                datasets: [
+                    {
+                    data: data.energy.data
+                    }
+                ]
+                }}
+                width={Dimensions.get("window").width} // from react-native
+                height={220}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={100} // optional, defaults to 1
+                chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                    borderRadius: 16
+                },
+                propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                }
+                }}
+                bezier
+                style={{
+                marginVertical: 8,
+                borderRadius: 16
+                }}
+            />
+        );
+    };
 
     // https://reactnative.dev/docs/flatlist
     // https://github.com/indiespirit/react-native-chart-kit
     return(
-        <View>
-            <LineChart
-            data={barData}
-            width={Dimensions.get("window").width} // from react-native
-            height={220}
-            yAxisLabel="$"
-            yAxisSuffix="k"
-            yAxisInterval={1} // optional, defaults to 1
-            chartConfig={{
-            backgroundColor: "#e26a00",
-            backgroundGradientFrom: "#fb8c00",
-            backgroundGradientTo: "#ffa726",
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-                borderRadius: 16
-            },
-            propsForDots: {
-                r: "6",
-                strokeWidth: "2",
-                stroke: "#ffa726"
-            }
-            }}
-            bezier
-            style={{
-            marginVertical: 8,
-            borderRadius: 16
-            }}
-        />
+        <View style={styles.container}>
+            { plot() }
         </View>
     );
     
@@ -98,6 +140,7 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         //alignContent: 'center'
+        marginTop: 50
     },
     header: {
         marginTop: '15%'
