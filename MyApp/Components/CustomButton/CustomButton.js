0647a3228c6625d84f837295ef5dcb41610f5155
muchomocho <ryo.mochizuki@city.ac.uk>
2022-04-03 07:08:38 +0100

    Update CustomButton.js

--- MyApp/Components/CustomButton/CustomButton.js
+++ MyApp/Components/CustomButton/CustomButton.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, TextInput, View, StyleSheet, Pressable } from 'react-native';
 
-export default function CustomButton({ onPress, text, buttonStyle, textStyle }) {
+export default function CustomButton({ onPress, text, isToggle=false, isHoldToggle=false, buttonStyle, textStyle }) {
+
+    const [state, setState] = useState({active: false});
+
+    const toggle = () => {
+        if (isToggle
+            && (!isHoldToggle || (isHoldToggle && !state.active))) {
+            setState({ active: !state.active});
+            console.log('yo', state)
+        }
+    };
+    
+    const holdToggle = () => {
+        if ((!isHoldToggle || isHoldToggle && !state.active)) {
+
+        }
+    };
 
     return (
-        <Pressable onPress={onPress} style={[styles.container, buttonStyle]}>
+        <Pressable 
+        onPress={() => {
+            onPress();
+            toggle(); 
+        }} 
+        style={
+            [
+                styles.container, 
+                buttonStyle, 
+                (state.active && isToggle) ? {backgroundColor: '#561ddbcc'} : {backgroundColor: '#561ddb'}
+            ]}
+        >
             <Text style={[styles.button, textStyle]}>{text}</Text>
         </Pressable>
     );
@@ -17,9 +44,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
 
+    activeButton: {
+        //backgroundColor: '#561ddbee',
+    },
+
     container: {
         width: '100%',
-        backgroundColor: '#561ddb',
+       //backgroundColor: '#561ddb',
         alignItems: 'center',
         padding: 15,
         marginVertical: 10,
