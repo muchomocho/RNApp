e900567a2693a5db4b005433948bb72727b5ba22
muchomocho <ryo.mochizuki@city.ac.uk>
2022-04-17 01:34:54 +0100

    refactoring + fix to show graph properly when there is no data from api

--- MyApp/Components/Chart/UserGraph.js
+++ MyApp/Components/Chart/UserGraph.js
@@ -4,15 +4,16 @@ import { VictoryBar, DataLabel, VictoryChart, VictoryTheme, VictoryLine, Victory
 import json from '../../assets/JSON/gov_diet_recommendation.json';
 import json_unit from '../../assets/JSON/gov_diet_recommendation_units.json';
 import CustomButton from "../CustomButton";
-import { dietDataContainer} from "../../Constant/Constant";
+import { useSelector, useDispatch } from 'react-redux';
+import { setSubuserArray, setCurrentSubuser, setUser } from '../../redux/actions'
 
-export default function UserGraph ({name, data, userData, dates}) {
+export default function UserGraph ({ data, dates }) {
 
     const windowWidth =  Dimensions.get('window').width
+    const { user, currentSubuser, subuserArray } = useSelector(state => state.userReducer);
 
     const [focusData, setFocusData] = useState('energy_kcal');
 
-    console.log(data)
     //const [data, setData] = useState({});
 
     // useEffect(() => {
@@ -59,7 +60,7 @@ export default function UserGraph ({name, data, userData, dates}) {
         if (age > 65 && age < 75) {
             return 65;
         }
-        if (age > 75) {
+        if (age >= 75) {
             return 75;
         }
         else return age;
@@ -70,24 +71,25 @@ export default function UserGraph ({name, data, userData, dates}) {
     }
 
     const datePoint = () => {
-        if (dates !== undefined && dates.length > 0 
-            && Object.prototype.hasOwnProperty.call(data, focusData)
+        if (dates !== undefined && dates.length > 0 ) {
+            const date = dates[dates.length - 1];
+            var index = -1
+            if (Object.prototype.hasOwnProperty.call(data, focusData) 
             && Object.prototype.hasOwnProperty.call(data, 'date')) {
-                const date = dates[dates.length - 1];
-                const index = data['date'].indexOf(date);
-                if (index > -1) {
-                    return [{ x: formatDate(date), y: parseFloat(data[focusData][index])}]; 
-                }
-                return [{ x: formatDate(date), y: 0 }]
-            
+                index = data['date'].indexOf(date);
+            }
+            if (index > -1) {
+                return [{ x: formatDate(date), y: parseFloat(data[focusData][index])}]; 
+            }
+            return [{ x: formatDate(date), y: 0 }]
         }
         return [];
     };
 
     const plotTarget = () => {
-        if (userData !== undefined && userData.age !== null && userData.gender !== null
+        if (currentSubuser !== undefined && (currentSubuser.age !== '' && currentSubuser.age !== null) && (currentSubuser.gender !== '' && currentSubuser.gneder !== null)
             && (dates !== undefined && dates.length > 0)) {
-                const line = parseFloat(json[ageMap(userData.age)][genderMap(userData.gender)][focusData]);
+                const line = parseFloat(json[ageMap(currentSubuser.age)][genderMap(currentSubuser.gender)][focusData]);
                 const lineData = dates.map(date => ({x: formatDate(date), y: line}));
                 console.log(lineData)
                 return (
@@ -100,7 +102,7 @@ export default function UserGraph ({name, data, userData, dates}) {
     };
 
     const plotDot = () => {
-        console.log(datePoint())
+        //console.log(datePoint())
         return (
             <VictoryBar
                 style={{data: {fill: "#0041b0"}}}
@@ -149,36 +151,38 @@ export default function UserGraph ({name, data, userData, dates}) {
 
     const plot = () => {
 
-        if (data !== undefined && Object.prototype.hasOwnProperty.call(data, focusData)) {
-            console.log('data',data)
-            const formattedData = () => {
-                var returnArray = new Array();
+        const formattedData = () => {
+             //console.log('data',data)
+            var returnArray = new Array();
 
-                dates.forEach(((element) => {
-                    const index = data['date'].indexOf(element)
-                    if (index > -1) {
-                        returnArray.push({ x: formatDate(element), y: parseFloat(data[focusData][index]) })
-                    }
-                    else {
-                        returnArray.push({ x: formatDate(element), y: 0 })
-                    }
-                }));
-                // data[focusData].forEach((element, index, array) => {
-                //     returnArray.push({ x: data['date'][index], y: element })
-                // });
-                console.log(returnArray)
-                return returnArray;
-            };
-            return (
-                
-                    <VictoryBar
-                        barWidth={windowWidth*0.04}
-                        style={{data: {fill: "#00b02f"}}}
-                        data={formattedData()}
-                        cornerRadius={styles.barCorner}
-                    />
-            );
-        }
+            dates.forEach(((element) => {
+                var index = -1
+                if (data !== undefined && Object.prototype.hasOwnProperty.call(data, focusData)) {
+                    index = data['date'].indexOf(element);
+                }
+                if (index > -1) {
+                    returnArray.push({ x: formatDate(element), y: parseFloat(data[focusData][index]) })
+                }
+                else {
+                    returnArray.push({ x: formatDate(element), y: 0 })
+                }
+            }));
+            // data[focusData].forEach((element, index, array) => {
+            //     returnArray.push({ x: data['date'][index], y: element })
+            // });
+            console.log('return', returnArray)
+            return returnArray;
+        };
+        return (
+            
+                <VictoryBar
+                    barWidth={windowWidth*0.04}
+                    style={{data: {fill: "#00b02f"}}}
+                    data={formattedData()}
+                    cornerRadius={styles.barCorner}
+                />
+        );
+        
     };
 
     const title = () => {
