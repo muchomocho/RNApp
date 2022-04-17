cda7ea2685e75b59a2ea76e910d33c2371abfd15
muchomocho <ryo.mochizuki@city.ac.uk>
2022-04-17 01:32:46 +0100

    added modal to display list of current list for submitting to record

--- MyApp/Screens/CreateRecord.js
+++ MyApp/Screens/CreateRecord.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { ActivityIndicator, StyleSheet, Text, View, Button, FlatList, Alert, Dimensions } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, Button, FlatList, Alert, Dimensions, Modal } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as APIRequest from '../API/ServerRequest';
 import { dietDataContainer } from "../Constant/Constant";
 import UserGraph from "../Components/Chart/UserGraph";
 import FoodDataSelection from "../Components/FoodData/FoodDataSelection";
+import CustomButton from "../Components/CustomButton";
+import FoodDataSelectionList from "../Components/FoodData/FoodDataSelectionList";
 
 function CreateRecord(props) {
+
+    const [isShowModal, setIsShowModal] = useState(false);
 
     // https://reactnavigation.org/docs/function-after-focusing-screen/
     useEffect(() => {
@@ -23,6 +27,23 @@ function CreateRecord(props) {
             <FoodDataSelection
             navigation={props.navigation}
             />
+            <CustomButton
+            buttonStyle={styles.button}
+            textStyle={styles.buttonText}
+            text={'list'}
+            onPress={()=>{setIsShowModal(!isShowModal)}}
+            />
+            <Modal
+            animationType="slide"
+            visible={isShowModal}>
+            <CustomButton
+            buttonStyle={styles.closeButton}
+            textStyle={styles.closeButtontext}
+            text={'close'}
+            onPress={()=>{setIsShowModal(!isShowModal)}}
+            />
+            <FoodDataSelectionList/>
+            </Modal>
         </View>
     );
     
@@ -33,6 +54,26 @@ const styles = StyleSheet.create({
         //backgroundColor: '#000',
         //alignContent: 'center'
         marginTop: 50,
+        height: '100%'
+    },
+    button: {
+        backgroundColor: '#fff',
+        position: 'absolute',
+        bottom: 50,
+        right: 20,
+        borderRadius: 45,
+        width: 70,
+        height: 70,
+        elevation: 10
+    },
+    buttonText: {
+        color: '#000'
+    },
+    closeButton: {
+        backgroundColor: '#ffffff00'
+    },
+    closeButtontext: {
+        color: '#000'
     },
     header: {
         marginTop: '15%'
