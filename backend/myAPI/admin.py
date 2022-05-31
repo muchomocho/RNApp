c127114093cbf4ac4ce03b037f491681c7dd1899
muchomocho <ryo.mochizuki@city.ac.uk>
2022-05-31 14:11:16 +0100

    final2

--- backend/myAPI/admin.py
+++ backend/myAPI/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from myAPI.models import Recipe, UserAccount
+from myAPI.models import *
 
 # Register your models here.
 
 admin.site.register(UserAccount)
+admin.site.register(Recipe)
+admin.site.register(Subuser)
+admin.site.register(FoodData)
+admin.site.register(UserMealRecord)
+admin.site.register(UserShareRequest)
+admin.site.register(UserMealRecordContent)
+admin.site.register(UserMealRecipeContent)
+admin.site.register(Recipe)
 admin.site.register(Recipe)
